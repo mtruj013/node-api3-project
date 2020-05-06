82e2c9e3fc6a6cd87fffcb4136e662ad6e206278
Maria Trujillo <mptruj015@gmail.com>
2020-05-07 00:04:55 +0200

    first 2 get requests and middleware function complete

--- server.js
+++ server.js
@@ -1,6 +1,14 @@
 const express = require('express');
-
 const server = express();
+
+//global middleware
+server.use(express.json());
+server.use(logger);
+
+//imports
+const userRouter = require('./users/userRouter.js');
+server.use('/api/users', userRouter);
+
 
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
@@ -8,6 +16,12 @@ server.get('/', (req, res) => {
 
 //custom middleware
 
-function logger(req, res, next) {}
+// logger logs to the console the following information about each request: request method, request url, and a timestamp
+// this middleware runs on every request made to the API
+function logger(req, res, next) {
+  console.log(`[${new Date().toISOString()}] ${req.meathod} to ${req.url} from ${req.get('Origin')}`);
+
+  next();
+}
 
 module.exports = server;
--- users/userRouter.js
+++ users/userRouter.js
@@ -1,5 +1,5 @@
 const express = require('express');
-
+const Users = require('./userDb.js')
 const router = express.Router();
 
 router.post('/', (req, res) => {
@@ -12,10 +12,35 @@ router.post('/:id/posts', (req, res) => {
 
 router.get('/', (req, res) => {
   // do your magic!
+  Users.get()
+  .then( user => {
+    res.status(200)
+    .json(user)
+  })
+  .catch( error => {
+    res.status(500)
+    .json({error: "The user information could not be retrieved."})
+  })
 });
 
 router.get('/:id', (req, res) => {
   // do your magic!
+  const id = req.params.id;
+
+  Users.getById(id)
+  .then( user =>{
+    if(user){
+      res.status(200)
+      .json(user)
+    } else {
+      res.status(404)
+      .json({message: "The post with the specified ID does not exist."})
+    }
+  })
+  .catch(error => {
+    res.status(500)
+    .json({error: "The information could not be retrieved."})
+  })
 });
 
 router.get('/:id/posts', (req, res) => {
@@ -24,16 +49,39 @@ router.get('/:id/posts', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   // do your magic!
+
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', validateUserId, (req, res) => {
   // do your magic!
+  const id = req.params.id;
+  const name = req.body
+
+  Users.update(id, name )
+      .then(userUpdate => {
+        res.status(200)
+        .json(userUpdate)
+      })
+      .catch(error => {
+        res.status(500)
+        .json({error: "The user could not be updated."})
+      })
 });
 
 //custom middleware
 
 function validateUserId(req, res, next) {
   // do your magic!
+  const id = req.params.id;
+  Users.getById(id)
+  .then(user => {
+    if(user){
+      next();
+    } else {
+      res.status(404)
+      .json({error: "The user with the specified ID does not exist."})
+    }
+  })
 }
 
 function validateUser(req, res, next) {
